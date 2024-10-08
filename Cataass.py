5a04a7a529d55adbcc3f2bef2b78b29f5d14eff3
Александр Розин <zak-24@yandex.ru>
2024-10-08 19:24:48 +0500

    Добавлено главное окно программы

---
+++ Cataass.py
@@ -0,0 +1,17 @@
+from tkinter import *
+
+
+def load_img(url):
+    pass
+
+w = Tk()
+w.title = ('Гыыыыы!')
+w.geometry()
+
+label = Label()
+label.pack()
+
+url = 'https://cataas.com/'
+img = load_img(url)
+
+w.mainloop()
